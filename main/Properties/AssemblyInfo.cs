41bdcf7dab1c78742bed2fad16dcdf9f5c7355d1
Denilson Rodrigues <jundenilson@gmail.com>
2018-10-08 18:06:16 -0300

    Nuget Things

--- main/Properties/AssemblyInfo.cs
+++ main/Properties/AssemblyInfo.cs
@@ -3,20 +3,20 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Security;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NPOI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Neuzilla")]
-[assembly: AssemblyProduct("NPOI")]
-[assembly: AssemblyCopyright("Apache 2.0")]
-[assembly: AssemblyTrademark("NPOI")]
+[assembly: AssemblyCompany("Inoa")]
+[assembly: AssemblyProduct("Inoa.NPOI")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
@@ -26,11 +26,11 @@ using System.Security;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.0.0")]
